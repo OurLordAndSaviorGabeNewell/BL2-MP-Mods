20d88c0a627f0ef67890adb387527f47c6b8bf7b
Rob Chiocchio <r.mchiocchio@gmail.com>
2018-09-24 12:59:02 -0400

    Basic unix patcher project base

---
+++ Patcher/Patcher-Unix/patcher_unix.py
@@ -0,0 +1,14 @@
+
+import click
+
+@click.command()
+@click.option('-g', '--game', default=2, help='Borderlands version. "--game 1" for Borderlands 1, "--game 2" for Borderlands 2, "--game 3" for Borderlands TPS')
+@click.option('--console-key', default='Tilde', help='Keybind to open console.')
+@click.option('--debug', is_flag=True, help='Enable debug mode. Do not do this unless you know what you are doing.')
+@click.argument('path', default='', type=click.Path(exists=True))
+
+def patch(game, consoleKey, debug, path):
+    click.echo('Hello, world!')
+
+if __name__ == '__main__':
+    patch()
