5202aec0917a2111616b168db109523997c84075
Rob Chiocchio <r.mchiocchio@gmail.com>
2018-09-25 11:03:27 -0400

    Set debug environment vars for debugging

--- Patcher/Patcher-Unix/patcher_unix.py
+++ Patcher/Patcher-Unix/patcher_unix.py
@@ -2,8 +2,8 @@
 import click
 
 @click.command()
-@click.option('-g', '--game', default=2, help='Borderlands version. "--game 1" for Borderlands 1, "--game 2" for Borderlands 2, "--game 3" for Borderlands TPS')
-@click.option('--console-key', default='Tilde', help='Keybind to open console.')
+@click.option('-g', '--game', default=2, type=int, help='Borderlands version. "--game 1" for Borderlands 1, "--game 2" for Borderlands 2, "--game 3" for Borderlands TPS')
+@click.option('consoleKey', '--console-key', default='Tilde', help='Keybind to open console.')
 @click.option('--debug', is_flag=True, help='Enable debug mode. Do not do this unless you know what you are doing.')
 @click.argument('path', default='', type=click.Path(exists=True))
 
